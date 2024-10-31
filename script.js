2c3e438fdc4a4aa82dbcc0a70aa8e2e355f00e77
siamanna <Sia.Manna@unh.edu>
2024-10-31 00:24:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
+
+searchForm.addEventListener("submit", (e) => {
+    
+})
